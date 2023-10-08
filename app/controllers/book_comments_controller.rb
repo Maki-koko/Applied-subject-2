86f5eb9fa382623abddbbda45ae0394350c67fb1
Maki-koko <144005937+Maki-koko@users.noreply.github.com>
2023-10-08 10:53:46 +0000

    コメント非同期完了

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,7 +2,7 @@ class BookCommentsController < ApplicationController
   
   def create
     @book = Book.find(params[:book_id])
-    @comment = current_user.book_comments.new(comment_params)
+    @comment = current_user.book_comments.new(book_comment_params)
     @comment.book_id=@book.id
     @comment.save
   end
@@ -17,7 +17,7 @@ class BookCommentsController < ApplicationController
   
   private
   
-  def comment_params
+  def book_comment_params
     params.require(:book_comment).permit(:comment)
     
   end
